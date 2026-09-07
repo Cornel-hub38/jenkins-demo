0f4514c9eb991afeb8c10670c4d48dfda63cdf00
Cornel <csaearle38@gmail.com>
2026-09-07 02:20:24 +0100

    Update demo message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from my Jenkins CI/CD application!"
+    return "Hello from my Jenkins CI/CD demo application!, this is my demo to community rev from Cornel Earle test1 auto"
 
 
 if __name__ == "__main__":
