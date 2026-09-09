b54515f9ec702813a2e784c639323e4e77507f0b
Cornel <csaearle38@gmail.com>
2026-09-09 17:50:11 +0100

    test3 atomate process

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from my Jenkins CI/CD demo application!, this is my demo to community rev from Cornel Earle test2 auto"
+    return "Hello from my Jenkins CI/CD demo application!, this is my demo to community rev from Cornel Earle test3 automate process"
 
 
 if __name__ == "__main__":
--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello from my Jenkins CI/CD demo application!, this is my demo to community rev from Cornel Earle test2 auto"
+    assert response.data == b"Hello from my Jenkins CI/CD demo application!, this is my demo to community rev from Cornel Earle test3 automate process"
